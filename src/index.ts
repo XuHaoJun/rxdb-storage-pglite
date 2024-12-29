9ad13ad9a0ae8f98436a681528070a6e0a16ae02
xuhaojun <xuhaojuntw@gmail.com>
2024-12-29 16:15:51 +0800

    feat: tidy export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 export * from './rx-storage-pglite';
-// export * from './pglite-types';
-// export * from './pglite-helper';
-// export * from './rx-storage-instance';
-
-export const placeholder = 'Hello TypeScript!';
+export * from './pglite-helper';
+export * from './rx-storage-instance';
